28a736a7b3e5b6249d754e6de4cd4954b378f1f9
Willy Horizont <willyhorizont@gmail.com>
2026-08-11 01:23:42 +0700

    [Last updated: 11 Aug 2026 @ 01:23 AM] version 0.0.6: update fastfetch config;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.0.5" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.0.6" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -13,8 +13,7 @@ version $V:
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-update freetext widget;
-update space available panel widget;
+update fastfetch config;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
