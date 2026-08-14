6dae17cbf93c0cee654f28bbc587c72a3ec6075f
willyhorizont <willyhorizont@gmail.com>
2026-08-14 18:38:11 +0700

    [Last updated: 14 Aug 2026 @ 06:38 PM] version 0.0.12 add dnf-conf.lnk; add fastestmirror;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.0.11" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.0.12" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
