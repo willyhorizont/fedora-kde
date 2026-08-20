b3283fda27716967b1caeb94a8c96bd8b2ecadc7
willyhorizont <willyhorizont@gmail.com>
2026-08-21 04:27:18 +0700

    [Last updated: 21 Aug 2026 @ 04:27 AM] version 0.0.17 update fastfetch-config.jsonc;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.0.16" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.0.17" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
