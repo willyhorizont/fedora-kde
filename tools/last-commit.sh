4ceb59b767edc4215cbf77bf14aaf1ed78e0085b
willyhorizont <willyhorizont@gmail.com>
2026-08-22 22:38:44 +0700

    [Last updated: 22 Aug 2026 @ 10:38 PM] version 0.0.26 update last-commit.sh; update katerc; remove FromKateToVscode.shortcuts;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.0.25" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.0.26" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -12,6 +12,7 @@ H="
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
+update last-commit.sh;
 update katerc;
 remove FromKateToVscode.shortcuts;
 "
@@ -24,5 +25,5 @@ git add .
 git commit -m "$M"
 git tag -d "$V" 2>/dev/null
 git tag -a "$V" -m "$M"
-git push origin main -f
-git push origin --tags -f
+git push origin main
+git push origin --tags
