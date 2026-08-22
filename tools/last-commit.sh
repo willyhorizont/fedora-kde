a8790156e70dda9818c2964a27d4f2bbbc88ca6d
willyhorizont <willyhorizont@gmail.com>
2026-08-22 13:06:07 +0700

    [Last updated: 22 Aug 2026 @ 01:06 PM] version 0.0.24 update .fresh/config.json; remove yazi.toml;

--- tools/last-commit.sh
+++ tools/last-commit.sh
@@ -2,7 +2,7 @@
 
 SD=$(dirname "$(realpath "$0")")
 RD=$(realpath "$SD/..")
-V="0.0.23" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
+V="0.0.24" # ! DON'T FORGET TO CHANGE VERSION BEFORE RUNNING !!!!
 T=$(date "+%d %b %Y @ %I:%M %p")
 cd "$RD" || exit
 
@@ -12,8 +12,8 @@ H="
 H=$(sed -e '/./,$!d' <<< "$H")
 # ! DON'T FORGET TO CHANGE COMMIT MESSAGE BEFORE RUNNING !!!!
 M="
-add .fresh/config.json;
-add fresh theme;
+update .fresh/config.json;
+remove yazi.toml;
 "
 M=$(sed -e '/./,$!d' <<< "$M")
 M="$H
